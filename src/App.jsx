0a4f076aa9e961fe0e3c3a08fa00994e5d578bb6
Mohamed Loay <elrabany1234@gmail.com>
2026-02-25 04:57:48 +0200

    fisrt commit

---
+++ src/App.jsx
@@ -0,0 +1,38 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className='mx-auto flex flex-col h-screen justify-center items-center font-cairo'>
+      <div className='flex gap-4'>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1 className='text-center font-bold text-3xl'>Vite + React</h1>
+      <div className="card text-center">
+        <button className='border border-gray-600 px-4 py-2 rounded-sm cursor-pointer' onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+      <p className="text-3xl">
+        اهلا بكم
+      </p>
+    </div>
+  )
+}
+
+export default App
