9f4ccbe946829b9596f1ab0fd79343548e422c59
Mohamed Loay <elrabany1234@gmail.com>
2026-03-05 17:36:57 +0200

    some taiwlind setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,19 +6,6 @@ export default {
   ],
   theme: {
     extend: {
-      colors:{
-        primary: '#6976EB',
-        secondary: '#ADB4F3',
-        title: '#3B3D53',
-        lightGray: '#B7B7B7',
-        red: '#FF0404',
-        danger: '#EAA1A1',
-        normal: '#EFD99A',
-        safe: '#97D799',
-        blue: '#2B3695',
-        navy: '#1F1A5F',
-        blueBlack: '#161A41',
-      },
       fontFamily: {
         sans: ["Cairo", "sans-serif"],
       },
