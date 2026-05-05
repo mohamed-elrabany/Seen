87d660d24e83e6c3464d0f9299e8b2aaaeac32da
Mohamed Loay <elrabany1234@gmail.com>
2026-05-05 17:54:23 +0300

    fixed deployment error again

---
+++ src/pages/logs/AddLog.jsx
@@ -0,0 +1,10 @@
+import GlucoseIcon from "../../components/ui/GlucoseIcon";
+import IconHeader from "../../components/ui/IconHeader";
+
+export default function AddLog() {
+    return(
+        <div>
+            <IconHeader icon={GlucoseIcon} title="Add Logs" />
+        </div>
+    );
+}
